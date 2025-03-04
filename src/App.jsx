a7f85432c329e87fd6353b8c0b7e9d5bab56c708
Maulana Ikhsan <anjay1435@gmail.com>
2025-03-04 16:21:18 +0700

    Adding Temporary Navbar , and some assets (Logo,profile images)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Logo from "../src/assets/logo_uika.png";
 
 function App() {
   const [npm, setNPM] = useState();
@@ -13,47 +13,50 @@ function App() {
   };
 
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <div className="bg-white p-8 rounded-2xl shadow-lg w-96 text-center">
-        <img src="/logo.png" alt="Logo" className="mx-auto mb-4 w-16" />
-        <h2 className="text-2xl font-bold text-green-800">S I A K A D</h2>
-        <p className="text-sm text-gray-600 mb-6">
-          Sistem Informasi Akademik dan Keuangan
-        </p>
-        <p className="text-sm text-gray-500 mb-4">
-          Untuk mengakses, dipersilahkan untuk login terlebih dahulu.
-        </p>
-        <div className="mb-4">
-          <input
-            type="text"
-            placeholder="NPM"
-            onChange={(e) => setNPM(e.target.value)}
-            className="w-full p-3 border rounded-lg focus:ring-2 focus:ring-green-500 focus:outline-none"
-          />
+    <div className="min-h-screen">
+      <Navbar />
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="bg-white p-8 rounded-2xl shadow-lg w-96 text-center">
+          <img src={Logo} alt="Logo" className="mx-auto mb-4 w-16" />
+          <h2 className="text-2xl font-bold text-green-800">S I A K A D</h2>
+          <p className="text-sm text-gray-600 mb-6">
+            Sistem Informasi Akademik dan Keuangan
+          </p>
+          <p className="text-sm text-gray-500 mb-4">
+            Untuk mengakses, dipersilahkan untuk login terlebih dahulu.
+          </p>
+          <div className="mb-4">
+            <input
+              type="text"
+              placeholder="NPM"
+              onChange={(e) => setNPM(e.target.value)}
+              className="w-full p-3 border rounded-lg focus:ring-2 focus:ring-green-500 focus:outline-none"
+            />
+          </div>
+          <div className="mb-4 relative">
+            <input
+              type="password"
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full p-3 border rounded-lg focus:ring-2 focus:ring-green-500 focus:outline-none"
+            />
+          </div>
+          <div className="flex items-center justify-between text-sm mb-4">
+            <label className="flex items-center space-x-2">
+              <input type="checkbox" className="form-checkbox text-green-600" />
+              <span>Ingat Saya</span>
+            </label>
+            <a href="#" className="text-green-600 hover:underline">
+              Lupa Password?
+            </a>
+          </div>
+          <button
+            onClick={submitHandler}
+            className="w-full bg-green-700 text-white p-3 rounded-lg hover:bg-green-800 transition"
+          >
+            Login
+          </button>
         </div>
-        <div className="mb-4 relative">
-          <input
-            type="password"
-            placeholder="Password"
-            onChange={(e) => setPassword(e.target.value)}
-            className="w-full p-3 border rounded-lg focus:ring-2 focus:ring-green-500 focus:outline-none"
-          />
-        </div>
-        <div className="flex items-center justify-between text-sm mb-4">
-          <label className="flex items-center space-x-2">
-            <input type="checkbox" className="form-checkbox text-green-600" />
-            <span>Ingat Saya</span>
-          </label>
-          <a href="#" className="text-green-600 hover:underline">
-            Lupa Password?
-          </a>
-        </div>
-        <button
-          onClick={submitHandler}
-          className="w-full bg-green-700 text-white p-3 rounded-lg hover:bg-green-800 transition"
-        >
-          Login
-        </button>
       </div>
     </div>
   );
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Logo from "../assets/logo_uika.png";
+import ProfileLogo from "../assets/profile_logo.png";
+
+const Navbar = () => {
+  return (
+    <div className="bg-primary-green text-white py-4 w-full">
+      <div className="container mx-auto max-w-6xl">
+        <div className="w-full py-2">
+          <div className="flex justify-between items-center">
+            <div className="flex gap-4 items-center">
+              <img
+                className="bg-white p-1 rounded-lg"
+                width={50}
+                src={Logo}
+                alt=""
+              />
+              <div>
+                <h1>SIM Akademik</h1>
+                <h1 className="font-semibold text-md">
+                  Universitan Ibn Khaldun
+                </h1>
+              </div>
+            </div>
+            <div className="flex space-x-4 items-center">
+              <h1>Bel</h1>
+              <img className="rounded-full" width={30} src={ProfileLogo} />
+            </div>
+          </div>
+        </div>
+        <ul className="flex space-x-12 text-white">
+          <li>Beranda</li>
+          <li>Jadwal</li>
+          <li>Akademik</li>
+          <li>Tingkat Akhir</li>
+          <li>Hasil Studi</li>
+          <li>Keuangan</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
