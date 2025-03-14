ce8bbaaf17653438c69528503bb3c4ac45bf7936
Firman Hakim <firmanhakim11704@gmail.com>
2025-03-14 13:26:22 +0700

    modifikasi login dan struktur projek

---
+++ src/components/FormLogin.tsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import React from "react";
+
+export default function FormLogin() {
+  const [npm, setNPM] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = () => {
+    console.log("NPM : ", npm);
+    console.log("Password : ", password);
+  };
+
+  return (
+    <form className="flex justify-center items-center min-h-screen">
+      <div className="bg-white p-8 rounded-2xl shadow-lg w-96 text-center">
+        <div className="flex items-center justify-center gap-5 mb-4">
+          <img src="/img/logo_uika.png" alt="Logo" className="w-8 md:w-9" />
+          <h2 className="text-2xl md:text-3xl font-bold text-[#198754]">S I A K A D</h2>
+        </div>
+
+        <p className="text-[11px] md:text-sm text-[#198754] mb-3 font-semibold">
+          Sistem Informasi Akademik dan Keuangan
+        </p>
+        <p className="text-[11px] md:text-sm text-gray-800 mb-3">
+          Untuk mengakses, dipersilahkan untuk login terlebih dahulu.
+        </p>
+        <div className="mb-4">
+          <input
+            type="text"
+            placeholder="NPM"
+            onChange={(e) => setNPM(e.target.value)}
+            className="w-full p-3 bg-[#DDDDDD] rounded-2xl focus:ring-2 focus:ring-green-500 focus:outline-none text-[11px] md:text-sm"
+          />
+        </div>
+        <div className="mb-4 relative">
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+            className="w-full p-3 bg-[#DDDDDD] rounded-2xl focus:ring-2 focus:ring-green-500 focus:outline-none text-[11px] md:text-sm"
+          />
+        </div>
+        <div className="flex items-center justify-between text-sm mb-4">
+          <label className="flex items-center space-x-2 text-gray-500 text-[11px]">
+            <input type="checkbox" className="form-checkbox text-green-600" />
+            <span>Ingat Saya</span>
+          </label>
+          <a href="#" className="text-green-600 hover:underline text-[11px]">
+            Lupa Password?
+          </a>
+        </div>
+        <button
+          onClick={submitHandler}
+          className="w-full bg-green-700 text-white p-2 text-sm rounded-lg hover:bg-green-800 transition"
+        >
+          Login
+        </button>
+      </div>
+    </form>
+  );
+}
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import FormLogin from "../components/FormLogin";
+
+export default function LoginPage() {
+  return (
+    <div className="min-h-screen relative">
+      {/* Background */}
+      <img
+        src="/img/background_login.png"
+        alt=""
+        className="absolute w-full h-full -z-10 filter brightness-50 object-cover"
+      />
+
+      <div className="px-7">
+        <FormLogin />
+      </div>
+    </div>
+  );
+}
