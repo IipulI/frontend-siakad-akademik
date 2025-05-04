de6ff90c5e43b92c96d303b05b1c9a9eea1ed151
Firman Hakim <firmanhakim11704@gmail.com>
2025-05-04 19:23:18 +0700

    memperbaiki tampilan di page retake

--- src/pages/studentModule/academic/Retake.tsx
+++ src/pages/studentModule/academic/Retake.tsx
@@ -1,29 +1,39 @@
 import React from "react";
 import Biodata from "../../../components/Biodata";
 import MainLayout from "../../../components/layouts/MainLayout";
-import Table from "../../../components/Table";
+import { Table } from "../../../components/Table";
 
 export default function Retake() {
   const tableHead = [
     "No",
     "Kode MK",
     "Nama Mata Kuliah",
-    "Kelas",
+    "Priode",
     "SKS",
-    "Hari",
-    "Jam",
-    "Ruangan",
-    "Dosen Pengajar",
+    "Semester",
+    "Nilai",
+  ];
+
+  const data = [
+    {
+      no: 1,
+      kodeMk: "TIF202",
+      mataKuliah: "Metode Penelitian",
+      priode: "2024 Genap",
+      sks: 3,
+      semester: "4",
+      nilai: "D",
+    },
   ];
 
   return (
     <MainLayout isGreeting={false} titlePage={"Mengulang"} className="">
-      <div className="w-full bg-white min-h-screen py-2 rounded-2xl border-t-2 border-primary-green">
-        <Biodata />
+      <div className="w-full bg-white min-h-screen py-4 rounded-sm border-t-2 border-primary-yellow">
+        <Biodata showLine={false} />
 
         <Table
           tableHead={tableHead}
-          data={[]}
+          data={data}
           error={"Mahasiswa tidak pernah mengulang mata kuiah"}
         />
       </div>
